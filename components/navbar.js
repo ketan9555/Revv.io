4a379c5bd48b1ce6ce5ec021af8982d09cff0237
Teju0910 <tejasvini_fw15_007@masai.school>
2022-02-24 14:41:55 +0530

    navbar submit

---
+++ components/navbar.js
@@ -0,0 +1,117 @@
+const navbar = () => {
+    return  `
+    <div id="navbar">
+    <div id="navleft">
+    <img id="logo" src="https://www.revv.co.in/grapheneImages/newopen/logo.svg"/>
+    <h6 style="margin-top: -12px;margin-left:42px ;">Sanitized. Safe</h5>
+    </div>
+    <div id="navmid">
+       <div  class="hun">
+           <p class="subs"><b>Hyundai</b>  Subcription</p>
+       </div>
+       <div class="hun">
+           <p class="subs"> <b>Mahindra </b> Subcription</p>
+       </div>
+    </div>
+    <div id="navright">       
+       <div style="margin-top: 10px;"> <b> FAQs </b></div>
+       <div id="logshow">
+           <img id="signlogo" src="https://www.revv.co.in/grapheneImages/newopen/ic-web-profile-login.svg"/>
+           <h4 id="x">Login or Signup</h4>
+       </div>
+    </div>
+   </div>
+   <br>
+
+
+
+<!-- login data -->
+
+<div id="login" class="loginstyle">
+       <div id="login-a">
+           <div id="login-b">
+               <form id="signin-details">
+                   <div id="b">
+                       <b id="login-title">Login</b>
+                       <i id="cross1" class="fa fa-duotone fa-xmark"></i>
+                   </div>
+                   <div>
+                       <input type="email" name="" class="indetails" id="outmail" placeholder="Enter Email"><br> <hr>
+                       <span class="valid" id="mymail">Please enter valid email id</span>
+                   </div>
+                   <br>
+                   <div>
+                       <input type="password" name="" class="indetails" id="outpass" placeholder="Enter Password"><hr>
+                       <span class="valid" id="mypass"> Send a new password to my email ID</span>
+                   </div>
+                   <button id="outbtn" type="submit"> Login</button>
+               </form>
+               <div >
+                   <p>Don't have a account yet?</p>
+                   <p id="gotosignup">Sign Up</p>
+               </div>
+               <hr>
+               <div id="google">
+                   <a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fwww.revv.co.in%3Fid%3Dauth616939&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&client_id=179663074908-qjbll6547bit8bpghq1aio472t7b7ufk.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fwww.revv.co.in&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow">
+                       <img src="https://www.revv.co.in/imgs/google-login.png" alt="">
+                   </a>
+               </div>
+               <div class="fb">              
+                   <a href="https://www.facebook.com/login.php?skip_api_login=1&api_key=844747322290622&kid_directed_site=0&app_id=844747322290622&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv2.1%2Fdialog%2Foauth%3Fapp_id%3D844747322290622%26cbt%3D1645524138719%26channel_url%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df2aeb26868930f8%2526domain%253Dwww.revv.co.in%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Fwww.revv.co.in%25252Ff28dea41af42b94%2526relation%253Dopener%26client_id%3D844747322290622%26display%3Dpopup%26domain%3Dwww.revv.co.in%26e2e%3D%257B%257D%26fallback_redirect_uri%3Dhttps%253A%252F%252Fwww.revv.co.in%252Fopen%252Fpune%252Fhyundai-subscription%252Fstock%252Fcars_pricing%26locale%3Den_US%26logger_id%3Df993922f3d2fac%26origin%3D1%26redirect_uri%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df25a117ef647e48%2526domain%253Dwww.revv.co.in%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Fwww.revv.co.in%25252Ff28dea41af42b94%2526relation%253Dopener%2526frame%253Df58db917250ffc%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26scope%3Demail%26sdk%3Djoey%26version%3Dv2.1%26ret%3Dlogin%26fbapp_pres%3D0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df25a117ef647e48%26domain%3Dwww.revv.co.in%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fwww.revv.co.in%252Ff28dea41af42b94%26relation%3Dopener%26frame%3Df58db917250ffc%26error%3Daccess_denied%26error_code%3D200%26error
+                   _description%3DPermissions%2Berror%26error_reason%3Duser_denied&display=popup&locale=en_GB&pl_dbl=0">
+                   <img src="https://www.revv.co.in/imgs/facebook-icon.png" alt="">
+               </a>
+               </div>              
+           </div>
+       </div>
+   </div>
+
+   <!-- signup data -->
+
+   <div id="signup" class="loginstyle">
+       <div id="login-a">
+           <div id="login-b">              
+               <form id="signup-details">
+                   <div id="b">
+                       <b id="login-title">Sign Up</b>
+                       <i id="cross2" class="fa fa-duotone fa-xmark"></i>
+                   </div>
+                   <div id="google">
+                       <a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fwww.revv.co.in%3Fid%3Dauth616939&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&client_id=179663074908-qjbll6547bit8bpghq1aio472t7b7ufk.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fwww.revv.co.in&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow">
+                           <img src="https://www.revv.co.in/imgs/google-login.png" alt="">  </a>
+                   </div>
+                   <div class="fb">              
+                       <a href="https://www.facebook.com/login.php?skip_api_login=1&api_key=844747322290622&kid_directed_site=0&app_id=844747322290622&signed_next=1&next=https%3A%2F%2Fwww.facebook.com%2Fv2.1%2Fdialog%2Foauth%3Fapp_id%3D844747322290622%26cbt%3D1645524138719%26channel_url%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df2aeb26868930f8%2526domain%253Dwww.revv.co.in%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Fwww.revv.co.in%25252Ff28dea41af42b94%2526relation%253Dopener%26client_id%3D844747322290622%26display%3Dpopup%26domain%3Dwww.revv.co.in%26e2e%3D%257B%257D%26fallback_redirect_uri%3Dhttps%253A%252F%252Fwww.revv.co.in%252Fopen%252Fpune%252Fhyundai-subscription%252Fstock%252Fcars_pricing%26locale%3Den_US%26logger_id%3Df993922f3d2fac%26origin%3D1%26redirect_uri%3Dhttps%253A%252F%252Fstaticxx.facebook.com%252Fx%252Fconnect%252Fxd_arbiter%252F%253Fversion%253D46%2523cb%253Df25a117ef647e48%2526domain%253Dwww.revv.co.in%2526is_canvas%253Dfalse%2526origin%253Dhttps%25253A%25252F%25252Fwww.revv.co.in%25252Ff28dea41af42b94%2526relation%253Dopener%2526frame%253Df58db917250ffc%26response_type%3Dtoken%252Csigned_request%252Cgraph_domain%26scope%3Demail%26sdk%3Djoey%26version%3Dv2.1%26ret%3Dlogin%26fbapp_pres%3D0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fstaticxx.facebook.com%2Fx%2Fconnect%2Fxd_arbiter%2F%3Fversion%3D46%23cb%3Df25a117ef647e48%26domain%3Dwww.revv.co.in%26is_canvas%3Dfalse%26origin%3Dhttps%253A%252F%252Fwww.revv.co.in%252Ff28dea41af42b94%26relation%3Dopener%26frame%3Df58db917250ffc%26error%3Daccess_denied%26error_code%3D200%26error
+                       _description%3DPermissions%2Berror%26error_reason%3Duser_denied&display=popup&locale=en_GB&pl_dbl=0">
+                       <img src="https://www.revv.co.in/imgs/facebook-icon.png" alt="">
+                   </a>
+                   </div>
+                   <div>
+                       <input type="text" class="indetails" id="innam" placeholder=" Name"><br>
+                       <hr>
+                       <span  class="valid" id="validnam">Please enter your name</span>
+                   </div><br>
+                   <div>
+                       <input type="email" name="" class="indetails" id="inmail" placeholder=" Email"><br>
+                       <hr>
+                       <span  class="valid" id="validmail">Please enter valid email-id</span>
+                   </div><br>
+                   <div>
+                       <input type="password" name="" class="indetails" id="inpass" placeholder="Password">
+                       <hr>
+                       <span class="valid" id="validpass"> Set a new password</span>
+                   </div><br>
+                   <div>
+                       <input type="number" name="" class="indetails" id="innum" placeholder=" Mobile Number">
+                       <hr>
+                       <span  class="valid" id="validnum"> Please enter your mobile number</span>
+                   </div>
+                   <button id="inputbtn" type="submit"> Proceed</button>
+               </form>               
+           </div>
+       </div>
+   </div>  `
+}
+
+
+export { navbar }
---
+++ index.js
@@ -0,0 +1,141 @@
+import { navbar }  from "./components/navbar.js";
+//console.log(navbar)
+
+let navbardiv = document.getElementById("container")
+navbardiv.innerHTML = navbar();
+
+let loginshow = document.getElementById("logshow")
+loginshow.onclick = function() {
+document.getElementById("login").style.display ="block"
+}
+
+document.querySelector("form").addEventListener("submit", signin);
+var AllUsers = JSON.parse(localStorage.getItem("userDataBase"));
+function signin(event){
+event.preventDefault();
+var mail = document.getElementById("outmail").value
+var pass = document.getElementById("outpass").value
+// console.log(regdUsers.username)
+  
+for(var i=0; i<AllUsers.length; i++)
+{
+    var flag=false;  
+    if(mail == []){
+        let text = document.getElementById("mymail");
+        text.style.color ="red";
+        flag=true;
+        break;
+   }
+   else if(mail !== []){
+    let text = document.getElementById("mymail");
+    text.style.color ="#0ebaba";
+    flag=true;
+   }
+     if(pass == []){
+        let text = document.getElementById("mypass");
+        text.style.color ="red";
+        flag=true;
+        break;
+   }
+    else  if(AllUsers[i].mail == mail && AllUsers[i].password == pass)
+      {
+        flag=true;
+        alert("login successfully")
+        document.getElementById("login").style.display ="none";
+        // document.getElementById("logshow").style.display ="none";
+        let showname = document.getElementById("x")
+        showname.innerHTML = AllUsers[i].username;
+
+        let showlogo = document.getElementById("signlogo")
+        showlogo.src ="https://www.revv.co.in/grapheneImages/newopen/ic-web-profile-nav.svg";
+        showname.setAttribute("id","namshow")
+        
+        showname.onclick = function () {
+
+        }
+        break;
+      }
+}
+if(flag==false){
+    alert("Invaild Username or Password") 
+  }
+
+}
+
+// signup ////
+let showsignup = document.getElementById("gotosignup")
+showsignup.onclick = function () {
+  document.getElementById("login").style.display ="none";
+  document.getElementById("signup").style.display ="block"
+}
+
+
+let startfill = document.getElementById("inputbtn");
+
+var userStack=JSON.parse(localStorage.getItem("userDataBase"))||[];
+
+startfill.onclick =  function formSubmit(){
+  event.preventDefault();
+
+  var nam = document.getElementById("innam").value 
+  var mail = document.getElementById("inmail").value 
+  var pass = document.getElementById("inpass").value 
+  var num = document.getElementById("innum").value 
+  console.log(nam,mail,pass,num)
+ 
+  var flag=true;
+  // if(pass !=="" || mail !== "" || nam !== "" || num !== "" ){
+  //   let text = document.querySelectorAll(".valid");
+  //   text.style.color = "green";
+  //   flag=true;
+  // }
+  if(nam == ""){
+      let text = document.getElementById("validnam");
+      text.style.color ="red";  
+      flag=false;
+  }
+
+  if(mail == ""){
+    let text = document.getElementById("validmail");
+    text.style.color ="red";
+    flag=false;
+  }
+
+  if(pass == ""){
+    let text = document.getElementById("validpass");
+    text.style.color ="red";
+    flag=false;
+  }
+
+  if(num == "" || num.length != 10 ){
+    let text = document.getElementById("validnum");
+    text.style.color ="red";
+    text.innerHTML ="*Invalid Mobile Number";
+    flag=false;
+  }
+  if(flag == true){
+      var userData = {
+          username: nam,
+          mail: mail,
+          password: pass,
+          mobile: num,
+      };
+      userStack.push(userData);
+      localStorage.setItem("userDataBase", JSON.stringify(userStack))
+      alert("Sign-up succsessfully...")  
+      document.getElementById("login").style.display ="block";
+      document.getElementById("signup").style.display ="none"
+   }   
+  }
+
+
+
+let displayin = document.getElementById("cross1")
+displayin.onclick = function close () {
+  document.getElementById("login").style.display ="none";
+}
+
+let displayup = document.getElementById("cross2")
+displayup.onclick = function close () {
+  document.getElementById("signup").style.display ="none"
+}
